0f52b450f539e4b9e2fb7a1f1df5cd4a83995136
Di-kairos <205350801+Di-kairos@users.noreply.github.com>
2026-08-03 22:50:16 +0300

    chore(formula): bump to v0.1.7

--- Formula/vaultwatch.rb
+++ Formula/vaultwatch.rb
@@ -2,7 +2,7 @@ class Vaultwatch < Formula
   desc "Guard an open securetrash vault on macOS (Spotlight/Time Machine/cloud)"
   homepage "https://github.com/Di-kairos/vaultwatch"
   url "https://github.com/Di-kairos/vaultwatch/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "ad278b03a37374d2db5399d34ea05fc27ca449e77c9515353edc7e2b26f7b477"
+  sha256 "5a8611edbe84e0dea1bc14b5ceb3368296adf75f39c2ca6c72f64db6dea5e6de"
   license "MIT"
 
   def install
