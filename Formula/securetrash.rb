e8942d33fb8a70b016a7fda459127049c5c8a8cf
Di-kairos <205350801+Di-kairos@users.noreply.github.com>
2026-08-05 22:06:48 +0300

    chore(formula): bump to v0.5.2

--- Formula/securetrash.rb
+++ Formula/securetrash.rb
@@ -1,8 +1,8 @@
 class Securetrash < Formula
   desc "Honest secure file deletion for macOS (FileVault + crypto-shred vaults)"
   homepage "https://github.com/Di-kairos/securetrash"
-  url "https://github.com/Di-kairos/securetrash/archive/refs/tags/v0.5.1.tar.gz"
-  sha256 "14cf9597e42dcb680c7689c0ee27d87ca92d94e8d3560dd4121ebadfd453a4aa"
+  url "https://github.com/Di-kairos/securetrash/archive/refs/tags/v0.5.2.tar.gz"
+  sha256 "ffd2d12a9a16e51dbca7f6f6a0dbb0013c8339c27cbeda0b0ef63b8f31d61a5d"
   license "MIT"
 
   def install
